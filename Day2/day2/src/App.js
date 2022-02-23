6a53f5640fa6e57437af2d57256d07464afc7728
edehemma09 <edehemmanuelebubechukwu@gmail.com>
2022-02-23 09:21:40 -0500

    updated commit

--- Day2/day2/src/App.js
+++ Day2/day2/src/App.js
@@ -3,6 +3,7 @@ import Profile from './component/Profile';
 import Comp1 from './component/Comp1';
 import Comp2 from './component/Comp2';
 import Comp3 from './component/Comp3';
+import Comp4 from './component/Comp4';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Comp1 CarName="Audi"/>
       <Comp2 VixeName="Mission"/>
       <Comp3 Gender="Unisex"/>
+      <Comp4 Data="Transport"/>
 
     </div>
   );
---
+++ Day2/day2/src/component/Comp4.js
@@ -0,0 +1,14 @@
+function Comp4 ({Data}) {
+  
+    return (
+      <div>
+        
+        <h2>Getting the customers {Data} </h2>
+        
+      </div>
+     
+    );
+ 
+  }
+  
+  export default Comp4;

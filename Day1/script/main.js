f3b2dc8a23eebf012f2328cf88bc9183d2e1aaeb
Edeh_Emma <edehemmanuelebubechukwu@gmail.com>
2022-02-24 10:50:49 +0100

    initial commit

---
+++ Day1/script/main.js
@@ -0,0 +1,14 @@
+function day1() {
+    var name = (document.getElementsByTagName('body')[0].innerHTML ='Testin the code1');
+    // Things to cover today inline comment,
+    /**comments, syntax and variables
+     * multi-line comment
+     */
+    // variables let,const,var
+    var micheal = 2;
+    var micheal = 2;
+    let divine = 2
+    let divine;
+}
+day1();
+function scope()

5f655404741708926b3f7281ebb8c6a26251eef6
edehemma09 <edehemmanuelebubechukwu@gmail.com>
2022-03-03 13:18:08 -0500

    initial commit

--- Day2/day2/src/component/Input.js
+++ Day2/day2/src/component/Input.js
@@ -1,23 +1,50 @@
 import './Style/Input.css';
+import React,{useState} from 'react';
 
 function Input() {
-  
+  const [name, setName]=useState ('');
+  const [stack, setStack]=useState ('');
+  const [contact, setContact]=useState ('');
+
+  const inputs =[ 
+    {
+    name:"Name",
+    func:()=>{},
+    placeholder:"Enter Name",
+    type:"text",
+  },
+
+  {
+    name:"Stack",
+    func:()=>{},
+    placeholder:"Stack",
+    type:"text",
+  },
+  {
+
+    name:"Contact",
+    func:()=>{},
+    placeholder:"Contact",
+    type:"number",
+  },
+ 
+]
     return (
       <div className="inp">
-        <div className='inpcont'>
-        <label className='label' >Name:</label>
-        <input type="text" className='input' id='name' placeholder='Enter name'>
-        </input></div>
-        <div className='inpcont'>
-        <label className='label' >Stack:</label>
-        <input type="text" className='input' id='stack' placeholder='Stack' >
-        </input></div>
-        <div className='inpcont'>
-        <label className='label' >Contact:</label>
-        <input type="text" className='input' id='contact' placeholder='Contact '>
-        </input></div>
-        
+        {inputs.map ((e,i)=>
+          {return (
+          <div className='inpcont'key={i} >
+        <label className='label' >{e.name}</label>
+        <input type="e.type" className='input' id='name' placeholder={e.placeholder}>
+        </input>
+        </div>
+        );
+      }
+      )
+    }
+         
       </div>
+
      
     );
  
